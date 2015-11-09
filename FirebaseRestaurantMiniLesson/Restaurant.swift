2be8fabed3c90b956ee9ffe36c6343aba99a1097
Jordan Nelson <nelsonjordan20@yahoo.com>
2015-11-09 13:25:05 -0700

    Restaurant Model Object

--- FirebaseRestaurantMiniLesson/Restaurant.swift
+++ FirebaseRestaurantMiniLesson/Restaurant.swift
@@ -7,3 +7,44 @@
 //
 
 import Foundation
+import Firebase
+
+
+class Restaurant {
+    
+    var name: String
+    var category: String
+    var address: String
+
+    init(dictionary:[String:AnyObject]) {
+        
+        guard let name = dictionary["name"] as? String,
+            let category = dictionary["category"] as? String,
+            let address = dictionary["address"] as? String else {
+                self.name = ""
+                self.category = ""
+                self.address = ""
+                return
+        }
+        self.name = name
+        self.address = address
+        self.category = category
+    }
+    
+    func dictionaryCopy() -> [String:AnyObject] {
+        return ["name":name,
+            "address": address,
+            "category": category]
+    }
+    
+}
+
+
+
+
+
+
+
+
+
+
