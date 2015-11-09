2a409eb86ce2958554bb21e49404eedaed0520b7
Jordan Nelson <nelsonjordan20@yahoo.com>
2015-11-09 13:25:17 -0700

    RestaurantController

--- FirebaseRestaurantMiniLesson/RestaurantController.swift
+++ FirebaseRestaurantMiniLesson/RestaurantController.swift
@@ -7,3 +7,71 @@
 //
 
 import Foundation
+import Firebase
+
+public let RestaurantsUpdatedNotification = "RestaurantsUpdatedNotificationName"
+
+class RestaurantController {
+    
+    static let sharedController = RestaurantController()
+    
+    var restaurants: [Restaurant] {
+        didSet {
+            let nc = NSNotificationCenter.defaultCenter()
+            nc.postNotificationName(RestaurantsUpdatedNotification, object:self)
+        }
+    }
+    
+    init() {
+        self.restaurants = []
+        self.loadRestaurants()
+    }
+    
+    
+    func loadRestaurants() {
+        let allRestaurantsRef = FirebaseController.restaurantBase
+        allRestaurantsRef.observeEventType(FEventType.Value, withBlock: { snapshot -> Void in
+            
+            if let restaurantDictionaries = snapshot.value as? [[String: AnyObject]] {
+                self.restaurants = restaurantDictionaries.map({Restaurant(dictionary: $0)})
+            }
+            
+            }) { (error) -> Void in
+                print(error.localizedDescription)
+        }
+    }
+    
+    func addRestaurant(restaurant:Restaurant) {
+        restaurants.append(restaurant)
+        saveToFirebase()
+    }
+    
+    func saveToFirebase() {
+        let restaurantDictionaries = self.restaurants.map({$0.dictionaryCopy()})
+        FirebaseController.restaurantBase.setValue(restaurantDictionaries)
+    }
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
