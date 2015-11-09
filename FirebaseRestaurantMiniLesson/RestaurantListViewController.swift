288772b35b822c51ce9496aeaaa0f1d2ac431631
Jordan Nelson <nelsonjordan20@yahoo.com>
2015-11-09 13:24:57 -0700

    RestaurantListVC

--- FirebaseRestaurantMiniLesson/RestaurantListViewController.swift
+++ FirebaseRestaurantMiniLesson/RestaurantListViewController.swift
@@ -10,26 +10,78 @@ import UIKit
 
 class RestaurantListViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        let nc = NSNotificationCenter.defaultCenter()
+        nc.addObserver(self, selector: "restaurantsUpdated:", name: RestaurantsUpdatedNotification, object: nil)
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    override func viewWillAppear(animated: Bool) {
+        tableView.reloadData()
     }
-    */
 
+    @IBAction func addButtonPressed(sender: UIBarButtonItem) {
+        presentAlertController()
+    }
+   
+    func presentAlertController() {
+        let alert = UIAlertController(title: "Add Restaurant", message: "Enter restaurant details below.", preferredStyle: UIAlertControllerStyle.Alert)
+        
+        let cancel = UIAlertAction(title: "Cancel", style: UIAlertActionStyle.Cancel, handler: nil)
+        alert.addAction(cancel)
+        
+        let save = UIAlertAction(title: "Save", style: UIAlertActionStyle.Default) { (action) -> Void in
+            if let textfields = alert.textFields,
+                let name = textfields[0].text,
+                let address = textfields[1].text,
+                let category = textfields[2].text {
+                    
+                    let restaurantDictionary = ["name":name, "address":address, "category":category]
+                    RestaurantController.sharedController.addRestaurant(Restaurant(dictionary: restaurantDictionary))
+            }
+            
+        }
+        alert.addAction(save)
+        
+        alert.addTextFieldWithConfigurationHandler { (nameField) -> Void in
+            nameField.placeholder = "Name"
+        }
+        alert.addTextFieldWithConfigurationHandler { (addressField) -> Void in
+            addressField.placeholder = "Address"
+        }
+        alert.addTextFieldWithConfigurationHandler { (categoryField) -> Void in
+            categoryField.placeholder = "Category"
+        }
+        
+        presentViewController(alert, animated: true, completion: nil)
+    }
+
+    
+    func restaurantsUpdated(notification: NSNotification) {
+        tableView.reloadData()
+    }
+}
+
+extension RestaurantListViewController: UITableViewDataSource, UITableViewDelegate {
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCellWithIdentifier("restaurantCell", forIndexPath: indexPath)
+        
+        let restaurant =  RestaurantController.sharedController.restaurants[indexPath.row]
+        
+        cell.textLabel?.text = restaurant.name
+        cell.detailTextLabel?.text = restaurant.address
+        
+        return cell
+        
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return RestaurantController.sharedController.restaurants.count
+    }
 }
