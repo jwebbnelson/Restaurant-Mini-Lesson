6a8b692ae84871fab2ea5427fb220f451e2c16bb
Jordan Nelson <nelsonjordan20@yahoo.com>
2015-11-09 13:24:17 -0700

    Remove initial VC

---
+++ FirebaseRestaurantMiniLesson/RestaurantController.swift
@@ -0,0 +1,9 @@
+//
+//  RestaurantController.swift
+//  FirebaseRestaurantMiniLesson
+//
+//  Created by Jordan Nelson on 11/9/15.
+//  Copyright © 2015 Jordan Nelson. All rights reserved.
+//
+
+import Foundation
